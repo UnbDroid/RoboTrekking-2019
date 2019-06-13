8fc6502d8aecbf9329385f86bfa7efe790132d9b
rafael-at97 <rafael.a.t97@gmail.com>
2019-06-12 21:41:38 -0300

    Codigo de controle geral atualizado

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
-#define ENCODER_L 1
-#define ENCODER_R 2
+#define ENCODER_L 2
+#define ENCODER_R 1
 
 #define MOTOR_LEFT 1<<0
 #define MOTOR_RIGHT 1<<1
 
 #define MAXIMUM_VOLTAGE 7
 #define MINIMUM_VOLTAGE 0
-#define SAMPLES 1000
-#define N_PERIODS 1
+#define SAMPLES 500
+#define N_PERIODS 3
 
 // Period of sampling in seconds
 #define PERIOD 0.02
@@ -27,6 +27,7 @@ extern "C" {
     #include <rc/pthread.h>
 }
 
+double ts[SAMPLES];
 double input[SAMPLES];
 pair<double, double> outputs[SAMPLES];
 
@@ -51,6 +52,9 @@ void motor_set_voltage(int motors, double voltage){
 void send_triangular_wave(){
     int i=0, out1, out2;
     double value = MINIMUM_VOLTAGE;
+    double time = 0;
+
+    uint64_t nanos_since_boot = rc_nanos_since_boot(), temp;
 
     double rate = (MAXIMUM_VOLTAGE-MINIMUM_VOLTAGE)/double(SAMPLES/(N_PERIODS*2));
 
@@ -66,9 +70,10 @@ void send_triangular_wave(){
 
         motor_set_voltage(MOTOR_LEFT|MOTOR_RIGHT, value);
     
-        out1 = rc_encoder_read(ENCODER_L);
-        out2 = rc_encoder_read(ENCODER_R);
+        out1 = rc_encoder_eqep_read(ENCODER_L);
+        out2 = rc_encoder_eqep_read(ENCODER_R);
 
+	    ts[i] = time;
         input[i] = value;
         outputs[i] = make_pair(out1, out2);
 
@@ -76,6 +81,13 @@ void send_triangular_wave(){
 
         // Ugly simulation of period
         rc_usleep(int(PERIOD*1e6));
+        
+        temp = rc_nanos_since_boot();
+        time += (temp-nanos_since_boot)/(double)1e9;
+        nanos_since_boot = temp;
+
+        if(rc_get_state() == EXITING)
+            break;
     }
 }
 
@@ -86,14 +98,14 @@ void save_to_file(){
     data_file.open("data.txt");
 
     for(;i<SAMPLES;i++){
-        data_file << input[i] << " " << outputs[i].first << " " << outputs[i].second << endl;
+        data_file << ts[i] << "," << input[i] << "," << outputs[i].first << "," << outputs[i].second << endl;
     }
 
     data_file.close();
 }
 
 int main(){
-    cout << "Lembrou de usar config-pin em todos os pinos?" << endl;
+    cout << "Lembrou de usar pin-config em todos os pinos?" << endl;
 
     if(rc_enable_signal_handler() == -1){
         return -1;

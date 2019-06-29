8f0b8067f1ac529d664e1d66b5ad5b2d144c85a5
rafael-at97 <rafael.a.t97@gmail.com>
2019-06-29 16:08:04 -0300

    Codigo com onda quadrada e novas leituras

--- main.cpp
+++ main.cpp
@@ -6,8 +6,11 @@
 
 #define MAXIMUM_VOLTAGE 7
 #define MINIMUM_VOLTAGE 0
-#define SAMPLES 500
+#define SAMPLES 600
 #define N_PERIODS 3
+
+// 0 for square, 1 for triangular wave
+#define TRIANGULAR 1
 
 // Period of sampling in seconds
 #define PERIOD 0.02
@@ -69,7 +72,7 @@ void send_triangular_wave(){
         }
 
         motor_set_voltage(MOTOR_LEFT|MOTOR_RIGHT, value);
-    
+
         out1 = rc_encoder_eqep_read(ENCODER_L);
         out2 = rc_encoder_eqep_read(ENCODER_R);
 
@@ -78,6 +81,50 @@ void send_triangular_wave(){
         outputs[i] = make_pair(out1, out2);
 
         value = value + rate;
+
+        // Ugly simulation of period
+        rc_usleep(int(PERIOD*1e6));
+        
+        temp = rc_nanos_since_boot();
+        time += (temp-nanos_since_boot)/(double)1e9;
+        nanos_since_boot = temp;
+
+        if(rc_get_state() == EXITING)
+            break;
+    }
+}
+
+void send_square_wave(){
+    int i=0, out1, out2;
+    double value, time=0;
+
+    uint8_t state = 0;
+
+    uint32_t samples_per_cycle = SAMPLES/N_PERIODS;
+
+    uint64_t nanos_since_boot = rc_nanos_since_boot(), temp;
+
+    // Starts low and each half-cycle change it's state
+    for(;i<SAMPLES;i++){
+        if(state){
+            value = MAXIMUM_VOLTAGE;
+        }
+        else{
+            value = MINIMUM_VOLTAGE;
+        }
+
+        motor_set_voltage(MOTOR_LEFT|MOTOR_RIGHT, value);
+
+        out1 = rc_encoder_eqep_read(ENCODER_L);
+        out2 = rc_encoder_eqep_read(ENCODER_R);
+
+	    ts[i] = time;
+        input[i] = value;
+        outputs[i] = make_pair(out1, out2);
+
+        if(i%(samples_per_cycle/2) == 0){
+            state = (state+1)%2;
+        }
 
         // Ugly simulation of period
         rc_usleep(int(PERIOD*1e6));
@@ -126,7 +173,11 @@ int main(){
     // Starts thread that sends info to the arduino, SCHED_OTHER is the common RR
     rc_pthread_create(&comm_thread, send_pwm, (void*)pwm_to_send, SCHED_OTHER, 0);
 
+    #if TRIANGULAR
     send_triangular_wave();
+    #else
+    send_square_wave();
+    #endif
 
     rc_set_state(EXITING);
 

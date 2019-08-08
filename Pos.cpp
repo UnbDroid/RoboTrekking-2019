90f2e7890b15250a16c90be6e0177bbcfd02baa0
Vieira <vieira.arimatea@gmail.com>
2019-08-08 15:49:38 -0300

    Create class for position

---
+++ Pos.cpp
@@ -0,0 +1,15 @@
+#include "Pos.h"
+
+Pos::Pos()
+{
+}
+
+Pos::Pos(float x, float y)
+{
+    this->x = x;
+    this->y = y;
+}
+
+Pos::~Pos()
+{
+}
---
+++ Pos.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class Pos
+{
+public:
+    float x, y;
+
+    Pos();
+    Pos(float x, float y);
+    ~Pos();
+};

768f2a8658deb7754c2cb311cb1022ad6d9e29a5
rafael-at97 <rafael.a.t97@gmail.com>
2019-09-13 14:07:52 -0300

    Novo controlador com distancia dos sensores

--- sensors.cpp
+++ sensors.cpp
@@ -60,6 +60,32 @@ static void mpu_turnoff(void){
     rc_mpu_power_off();
 }
 
+static double get_distance_from_speed(double simulated_speed){
+    // Apply simpsons 1/3 rule for 3 points to get distance walked
+    static double speed[3] = {0.0, 0.0, 0.0}, time[3] = {0.0, 0.0, 0.0};
+    static index integrate_idx(3, 0);
+
+    static double total_distance = 0.0;
+
+    double integral;
+
+    // Make time readings (uint64_t) into double. Unit = seconds
+    time[integrate_idx.idx()] = (1.0*rc_nanos_since_boot())/1e9;
+
+    // Update speed value. Unit = meters per second
+    speed[integrate_idx.idx()] = simulated_speed/TERRAIN_FACTOR;
+
+    // Simpsons method
+    integral = speed[integrate_idx.idx(-2)] + 4*speed[integrate_idx.idx(-1)] + speed[integrate_idx.idx()];
+    integral = ( (time[integrate_idx.idx()] - time[integrate_idx.idx(-2)])/6) * (integral);
+    
+    total_distance += integral;
+
+    integrate_idx++;
+
+    return total_distance;
+}
+
 void* filter_sensors(void *arg){
     if(rc_enable_signal_handler() == -1){
         return NULL;
@@ -74,6 +100,8 @@ void* filter_sensors(void *arg){
     condition_variable* control_cv = args->arg_control_cv;
 
     mutex* navigation_mutex = args->arg_navigation_mutex;
+
+    double distance_walked;
 
     #if USING_ENCODER
     // Speed related variables
@@ -91,6 +119,8 @@ void* filter_sensors(void *arg){
     uint64_t time_spd, time_ref_spd = rc_nanos_since_boot();
 
     uint64_t measure_period = (CONTROLLER_PERIOD/20)*1e9; // 20 measures per controller loop, in nanoseconds
+    #else
+    double vel_l, vel_r;
     #endif
 
     // MPU start
@@ -112,7 +142,7 @@ void* filter_sensors(void *arg){
         // Get time of readings
         time_spd = rc_nanos_since_boot();
 
-        // time to measure?
+        // time to measure speed?
         if( (time_spd - time_ref_spd) > measure_period ){ 
 
             // Raw reading from encoders
@@ -150,18 +180,35 @@ void* filter_sensors(void *arg){
             // Update reference of time
             time_ref_spd = time_spd;
         }
+        
+        // Estimate distance trough encoders, the right one is not working :(
+        distance_walked = enc_l[enc_idx.idx()];
+
+        #else
+        // Get simulated speed to estimate distance walked
+        control_lock.lock();
+
+        // Using this variables to avoid creating new ones and overusing memmory
+        vel_l = readings[2];
+        vel_r = readings[3];
+        
+        distance_walked = get_distance_from_speed( (vel_l + vel_r)/2 );
+
+        control_lock.unlock();
         #endif
 
-        // Integrate accelerometer and encoders result and save into readings[0]
+        // Already lock mutex for safety
+        navigation_lock.lock();
+
+        // Get distance somehow (encoder data or speed integration) and save into readings[0]
         //      meters (m)
+        readings[0] = distance_walked;
 
         // Integrate gyro and store into readings[1], integration done by mpu dmp mode
         //      degrees (º)
-        navigation_lock.lock();
-
-        //readings[1] = mpu_data.dmp_TaitBryan[TB_YAW_Z]*RAD_TO_DEG;
         readings[1] = mpu_data.dmp_TaitBryan[TB_YAW_Z]*RAD_TO_DEG - drift;
 
+        // End of critic section
         navigation_lock.unlock();
 
         // Sleep for some time (0.005 seconds)
